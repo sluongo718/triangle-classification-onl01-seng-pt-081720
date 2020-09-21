355e05647411b021c3a8a25a4522416c9cf80cc4
sam l <sluongo718@yahoo.com>
2020-09-21 14:49:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
   # write code here
+  
+    attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    
+  end
+  
+  def kind 
+    if 
+      
+      
+      
+      
+      
+  end
+  end
+  
+    class TriangleError < StandardError
+    # triangle error code
+  end
+  
+  
 end
